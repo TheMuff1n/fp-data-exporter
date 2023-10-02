6046ece0e6b302f64746454c2ca451c8751b1aed
Marvin Stier <email@marvinstier.de>
2023-10-02 09:12:39 +0200

    removed appsettings.json in favor of cli args

--- exporter/Program.cs
+++ exporter/Program.cs
@@ -1,36 +1,76 @@
-﻿using ConsoleTables;
+﻿using CommandLine;
+using ConsoleTables;
 using exporter.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
-IConfiguration configuration = new ConfigurationBuilder()
-    .SetBasePath(Environment.CurrentDirectory)
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-string? connectionString = configuration.GetConnectionString("MyDatabase");
-
-var options = new DbContextOptionsBuilder<MyDBContext>()
-    .UseSqlServer(connectionString)
-    .Options;
-
-using (var dbContext = new MyDBContext(options))
+class Program
 {
-    var sensorsWithMeasurementCounts = dbContext.Sensor
-        .Select(sensor => new
-        {
-            sensor.DomainId,
-            sensor.Type.ValueType,
-            MeasurementsCount = sensor.Measurements.Count()
-        })
-        .ToList();
-
-    var table = new ConsoleTable("DomainId", "ValueType", "MeasurementsCount");
-
-    foreach (var s in sensorsWithMeasurementCounts)
+    class Options
     {
-        table.AddRow(s.DomainId, s.ValueType, s.MeasurementsCount);
+        [Value(0, MetaName = "connectionString", HelpText = "Connection string for the database connection.", Required = true)]
+        public string ConnectionString { get; set; }
+
+        [Option('f', "format", Default = OutputFormat.Preview, HelpText = "Output format (csv or human-readable preview).")]
+        public OutputFormat Format { get; set; }
+
+        [Option("from", HelpText = "Starting date to fetch data from.")]
+        public DateTime FromDate { get; set; } = DateTime.MinValue;
+
+        [Option("to", HelpText = "Ending date to fetch data from.")]
+        public DateTime ToDate { get; set; } = DateTime.MaxValue;
     }
 
-    table.Write();
+    enum OutputFormat
+    {
+        Preview,
+        Csv
+    }
+
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(Run)
+            .WithNotParsed(HandleParseError);
+    }
+
+    static void Run(Options options)
+    {
+        Console.WriteLine($"Loading data from source '{options.ConnectionString}'.");
+        Console.WriteLine($"Displaying data from {options.FromDate} to {options.ToDate} formatted as {options.Format}.");
+
+        var dbOptions = new DbContextOptionsBuilder<MyDBContext>()
+            .UseSqlServer(options.ConnectionString)
+            .Options;
+
+        using (var dbContext = new MyDBContext(dbOptions))
+        {
+            var sensorsWithMeasurementCounts = dbContext.Sensor
+                .Select(sensor => new
+                {
+                    sensor.DomainId,
+                    sensor.Type.ValueType,
+                    MeasurementsCount = sensor.Measurements
+                        .Where(m => options.FromDate <= m.MeasuredAt && m.MeasuredAt <= options.ToDate)
+                        .Count()
+                })
+                .ToList();
+
+            var table = new ConsoleTable("DomainId", "ValueType", "MeasurementsCount");
+
+            foreach (var s in sensorsWithMeasurementCounts)
+            {
+                table.AddRow(s.DomainId, s.ValueType, s.MeasurementsCount);
+            }
+
+            table.Write();
+        }
+    }
+
+    static void HandleParseError(IEnumerable<Error> errors)
+    {
+        foreach (var error in errors)
+        {
+            Console.WriteLine($"Error: {error}");
+        }
+    }
 }
