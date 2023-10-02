b96a3ea9caaf7db845ca6902a6309971b164e239
Marvin Stier <email@marvinstier.de>
2023-10-02 11:55:06 +0200

    added sensor selection and data preview

--- exporter/Program.cs
+++ exporter/Program.cs
@@ -10,7 +10,10 @@ class Program
         [Value(0, MetaName = "connectionString", HelpText = "Connection string for the database connection.", Required = true)]
         public string ConnectionString { get; set; }
 
-        [Option('f', "format", Default = OutputFormat.Preview, HelpText = "Output format (csv or human-readable preview).")]
+        [Option("sensors", HelpText = "Sensors to fetch data from, seperated by commas (e.g. 5000:10,5000:130).")]
+        public IEnumerable<string> SelectedSensors { get; set; }
+
+        [Option('f', "format", Default = OutputFormat.Preview, HelpText = "Output format (Summary, Preview or Csv).")]
         public OutputFormat Format { get; set; }
 
         [Option("from", HelpText = "Starting date to fetch data from.")]
@@ -22,6 +25,7 @@ class Program
 
     enum OutputFormat
     {
+        Summary,
         Preview,
         Csv
     }
@@ -37,6 +41,7 @@ class Program
     {
         Console.WriteLine($"Loading data from source '{options.ConnectionString}'.");
         Console.WriteLine($"Displaying data from {options.FromDate} to {options.ToDate} formatted as {options.Format}.");
+        Console.WriteLine($"Selected sensors: {(options.SelectedSensors.Count() == 0 ? "All" : string.Join(", ", options.SelectedSensors))}.");
 
         var dbOptions = new DbContextOptionsBuilder<MyDBContext>()
             .UseSqlServer(options.ConnectionString)
@@ -44,25 +49,15 @@ class Program
 
         using (var dbContext = new MyDBContext(dbOptions))
         {
-            var sensorsWithMeasurementCounts = dbContext.Sensor
-                .Select(sensor => new
-                {
-                    sensor.DomainId,
-                    sensor.Type.ValueType,
-                    MeasurementsCount = sensor.Measurements
-                        .Where(m => options.FromDate <= m.MeasuredAt && m.MeasuredAt <= options.ToDate)
-                        .Count()
-                })
-                .ToList();
-
-            var table = new ConsoleTable("DomainId", "ValueType", "MeasurementsCount");
-
-            foreach (var s in sensorsWithMeasurementCounts)
+            switch (options.Format)
             {
-                table.AddRow(s.DomainId, s.ValueType, s.MeasurementsCount);
+                case OutputFormat.Summary:
+                    ShowSummary(dbContext, options.FromDate, options.ToDate); break;
+                case OutputFormat.Preview:
+                    ShowPreview(dbContext, options.FromDate, options.ToDate, options.SelectedSensors); break;
+                case OutputFormat.Csv:
+                    SaveCsv(dbContext, options.FromDate, options.ToDate);  break;
             }
-
-            table.Write();
         }
     }
 
@@ -72,5 +67,83 @@ class Program
         {
             Console.WriteLine($"Error: {error}");
         }
+    }
+
+    static void ShowSummary(MyDBContext dbContext, DateTime fromDate, DateTime toDate)
+    {
+        var sensorsWithMeasurementCounts = dbContext.Sensor
+            .Select(sensor => new
+            {
+                sensor.DomainId,
+                sensor.Type.ValueType,
+                MeasurementsCount = sensor.Measurements
+                .Where(m => fromDate <= m.MeasuredAt && m.MeasuredAt <= toDate)
+                .Count()
+            })
+            .ToList();
+
+        var table = new ConsoleTable("DomainId", "ValueType", "MeasurementsCount");
+
+        foreach (var s in sensorsWithMeasurementCounts)
+        {
+            table.AddRow(s.DomainId, s.ValueType, s.MeasurementsCount);
+        }
+
+        table.Write();
+    }
+
+    static void ShowPreview(MyDBContext dBContext, DateTime fromDate, DateTime toDate, IEnumerable<string> selectedSensors)
+    {
+        var sensorsWithMeasurements = dBContext.Sensor
+            .Where(s => selectedSensors.Count() == 0 || selectedSensors.Contains(s.DomainId))
+            .Select(s => new
+            {
+                s.DomainId,
+                Measurements = s.Measurements
+                    .Where(m => !m.IsObsolete && fromDate <= m.MeasuredAt && m.MeasuredAt < toDate)
+                    .Select(m => new
+                    {
+                        m.MeasuredAt,
+                        m.MeasuredValue
+                    })
+            })
+            .ToList();
+
+        //TODO:set from and to to 15 minute fixed
+        var timestamps = new List<DateTime>();
+        var currentTimestamp = fromDate;
+        while (currentTimestamp < toDate)
+        {
+            timestamps.Add(currentTimestamp);
+            currentTimestamp = currentTimestamp.AddMinutes(15);
+        }
+
+        var tableHead = sensorsWithMeasurements
+            .Select(s => s.DomainId)
+            .Prepend("MeasuredAt")
+            .ToArray();
+
+        var tableBody = timestamps.Select(t => new
+        {
+            MeasuredAt = t,
+            Measurements = sensorsWithMeasurements
+                .Select(s => s.Measurements.FirstOrDefault(m => m.MeasuredAt == t)?.MeasuredValue)
+                .ToList()
+        })
+            .ToList();
+
+        var table = new ConsoleTable(tableHead);
+
+        foreach (var r in tableBody)
+        {
+            table.AddRow(r.Measurements.Select(m => string.Format("{0:F2}", m)).Prepend(r.MeasuredAt.ToString()).ToArray());
+        }
+
+        table.Write();
+    }
+
+    static void SaveCsv(MyDBContext dBContext, DateTime fromDate, DateTime toDate)
+    {
+        //TODO
     }
 }
